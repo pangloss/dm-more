1a28be6657e4a630b31e1bcaa9b194b99f89b83b
Darrick Wiebe <darrick@innatesoftware.com>
2008-07-14 02:50:22 -0400

    Added lib/dm-more to support requiring the embedded gems directly

---
+++ lib/dm-more.rb
@@ -0,0 +1,12 @@
+# Requiring dm-more itself adds its payload of gems directly to the path. That
+# allows them to be required without being installed as gems.  This is
+# especially useful when vendoring dm-more, and more specifically to prevent
+# versioning problems if this gem is updated but "rake install" has not been
+# rerun on the gems it contains.
+
+dir = File.join(Pathname(__FILE__).dirname.expand_path, '..')
+
+Dir[File.join(dir, '*')].each do |dm_gem|
+  $:.unshift(File.join(dm_gem, 'lib'))
+end
+
